14d454d6d6e11bd9c561788dddba915eec3e94d9
B3ni <90302291+B3ni15@users.noreply.github.com>
2024-05-22 11:45:54 +0200

    chore: Update webview_flutter dependency and clear cache
    This commit updates the webview_flutter dependency to the latest version and adds a clear cache functionality to the WebView. This ensures that the app is using the most up-to-date version of the package and improves performance by clearing the cache when the WebView is created.

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:io';
 
-class WebViewExample extends StatelessWidget {
+class WebViewExample extends StatefulWidget {
+  @override
+  _WebViewExampleState createState() => _WebViewExampleState();
+}
+
+class _WebViewExampleState extends State<WebViewExample> {
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) {
+      WebView.platform = SurfaceAndroidWebView();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WebView Example'),
+      ),
       body: WebView(
         initialUrl: 'https://app.dachats.online',
         javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) async {
+          await webViewController.clearCache();
+        },
       ),
     );
   }
