08e002ee41d56f30a33bf885f17f99d5d8251bb9
B3ni <xbenitmx2020@gmail.com>
2024-05-27 19:38:23 +0200

    Add EAS configuration and update dependencies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
-import { useEffect } from 'react';
-import { SafeAreaView, StyleSheet, View, Text, Linking } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default class HomeScreen extends Component {
   render() {
     return (
       <WebView
-        source={{uri: 'https://app.dachats.online/'}}
-        style={{marginTop: 20}}
+        source={{ uri: 'https://app.dachats.online/' }}
       />
     );
   }
