46e00b433c6392d7d7356c6cd7f7f79f4fd8773a
B3ni <90302291+B3ni15@users.noreply.github.com>
2024-06-14 10:11:40 +0200

    chore: Update react-native-permissions dependency to version 4.1.5
    This commit updates the react-native-permissions dependency to version 4.1.5. This update ensures that the project is using the latest stable version of the package, which includes bug fixes and improvements.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import { WebView } from 'react-native-webview';
+import { Platform, PermissionsAndroid } from 'react-native';
 
-export default class HomeScreen extends Component {
+class HomeScreen extends Component {
+  componentDidMount() {
+    this.requestMicrophonePermission();
+  }
+
+  async requestMicrophonePermission() {
+    try {
+      if (Platform.OS === 'android') {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+          {
+            title: 'Mikrofon Engedély',
+            message: 'Az alkalmazás használatához szükség van a mikrofon hozzáférésére.',
+            buttonPositive: 'Engedélyezés',
+          },
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          console.log('Mikrofon engedély megadva');
+        } else {
+          console.log('Mikrofon engedély megtagadva');
+        }
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  }
+
   render() {
-    return (
-      <WebView
-        source={{ uri: 'https://app.dachats.online/' }}
-      />
-    );
+    return <WebView source={{ uri: 'https://app.dachats.online/' }} />;
   }
 }
+
+export default HomeScreen;
